e2e88d778ef1bd72188844eeac0c67183358620b
darthjee <darthjee@gmail.com>
2025-08-18 17:28:32 -0300

    Add spectation

--- spec/lib/kiroshi/filters_spec.rb
+++ spec/lib/kiroshi/filters_spec.rb
@@ -166,5 +166,39 @@ RSpec.describe Kiroshi::Filters, type: :model do
         .from(scope).to(scope.where(name: name))
       end
     end
+
+    context 'when adding a filter with table qualification' do
+      let(:scope) { Document.joins(:tags) }
+
+      it do
+        expect { filters_class.filter_by :name, table: :documents }
+        .to change { filter_instance.apply(scope) }
+        .from(scope).to(scope.where(documents: { name: name }))
+      end
+    end
+
+    context 'when adding a filter with different table' do
+      let(:scope) { Document.joins(:tags) }
+      let(:filters) { { name: 'ruby' } }
+      let(:name) { 'ruby' }
+
+      it do
+        expect { filters_class.filter_by :name, table: :tags }
+        .to change { filter_instance.apply(scope) }
+        .from(scope).to(scope.where(tags: { name: name }))
+      end
+    end
+
+    context 'when adding a like filter with table qualification' do
+      let(:scope) { Document.joins(:tags) }
+      let(:filters) { { name: 'test' } }
+      let(:name) { 'test' }
+
+      it do
+        expect { filters_class.filter_by :name, match: :like, table: :documents }
+        .to change { filter_instance.apply(scope) }
+        .from(scope).to(scope.where('documents.name LIKE ?', '%test%'))
+      end
+    end
   end
 end
