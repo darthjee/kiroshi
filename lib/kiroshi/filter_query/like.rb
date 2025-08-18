864920a2e1f7e34fc2ae75a2ebfc7e91e17c4d33
darthjee <darthjee@gmail.com>
2025-08-18 20:23:38 -0300

    Fix AI mess

--- lib/kiroshi/filter_query/like.rb
+++ lib/kiroshi/filter_query/like.rb
@@ -32,18 +32,10 @@ module Kiroshi
       #
       # @since 0.1.1
       def apply
-        if respond_to?(:arel_table)
-          scope.where(
-            arel_table[attribute].matches(
-              "%#{value}%"
-            )
-          )
-        else
-          scope.where(
-            "#{table_name}.#{attribute} LIKE ?",
-            "%#{value}%"
-          )
-        end
+        scope.where(
+          "#{table_name}.#{attribute} LIKE ?",
+          "%#{value}%"
+        )
       end
     end
   end
